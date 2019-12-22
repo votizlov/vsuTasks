70dcb4c917ecbea0ef7a2f6b35b2381535f607d7
Votizlov <SeriousFox@yandex.ru>
2019-12-23 01:28:20 +0300

    added retrofit

--- src/main/java/NetworkService.java
+++ src/main/java/NetworkService.java
@@ -11,7 +11,6 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
-
 import javax.swing.plaf.synth.SynthLookAndFeel;
 import java.io.FileNotFoundException;
 import java.io.IOException;
